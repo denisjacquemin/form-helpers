3e05c20a852c4c9e61569aafa0caf45f8f88b1c1
Denis Jacquemin <denis.jacquemin@gmail.com>
2009-04-30 21:46:49 +0200

    label replaced by custom

--- lib/custom_form_helpers.rb
+++ lib/custom_form_helpers.rb
@@ -2,9 +2,9 @@ require 'denisj/customform/form_builder'
 require 'denisj/customform/form_helper'
 
 ActionView::Helpers::FormHelper.send(:include, DenisJ::CustomForm::FormHelper)
-ActionView::Helpers::FormHelper.alias_method_chain :method_missing, :label
+ActionView::Helpers::FormHelper.alias_method_chain :method_missing, :custom
 
 ActionView::Base.send(:include, DenisJ::CustomForm::FormHelper)
 
 ActionView::Helpers::FormBuilder.send(:include, DenisJ::CustomForm::FormBuilder)
-ActionView::Helpers::FormBuilder.alias_method_chain :method_missing, :label
+ActionView::Helpers::FormBuilder.alias_method_chain :method_missing, :custom
