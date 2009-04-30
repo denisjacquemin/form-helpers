4cb66871e8f83e2e065226d2323a29b3e8aa2405
Denis Jacquemin <denis.jacquemin@gmail.com>
2009-04-30 07:45:24 +0200

    README renamed and form_builder / form_helper cleanup

--- lib/denisj/customform/form_builder.rb
+++ lib/denisj/customform/form_builder.rb
@@ -1,7 +1,7 @@
 module DenisJ
   module CustomForm
     module FormBuilder
-      
+
       def method_missing_with_label(method_name, *args)
         if method_name.to_s =~ /^custom_/
           @template.send(method_name, @object_name, *args)
@@ -9,7 +9,7 @@ module DenisJ
           method_missing_without_label(method_name,*args)
         end
       end
-      
+
     end
   end
 end
